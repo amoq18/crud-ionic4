86e5e2db56680b3246c5553a82134dcfb4928f4d
Stagiaire <stagiaire@digitalafrique.org>
2020-03-08 14:28:58 +0100

    list and delete data

--- src/app/crudTuto/student-list/student-list.page.ts
+++ src/app/crudTuto/student-list/student-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListPage implements OnInit {
 
-  constructor() { }
+  studentsData: any;
+
+  constructor(
+    public apiService: ApiService
+  ) {
+    this.studentsData = [];
+  }
 
   ngOnInit() {
+    this.getAllStudents();
+  }
+
+  getAllStudents() {
+    //Get saved list of students
+    this.apiService.getList().subscribe(response => {
+      console.log(response);
+      this.studentsData = response;
+    })
+  }
+
+
+  delete(item) {
+    //Delete item in Student data
+    this.apiService.deleteItem(item.id).subscribe(Response => {
+      //Update list after delete is successful
+      this.getAllStudents();
+    });
   }
 
 }
